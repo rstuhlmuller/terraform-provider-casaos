9dc7d5f56b7542f6351f163d03bf8aa9607f9898
Rodman <rodman@stuhlmuller.net>
2024-04-15 16:17:30 -0700

    fix: deprecated

--- internal/client/client.go
+++ internal/client/client.go
@@ -2,7 +2,7 @@ package casaos
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"time"
 )
@@ -78,7 +78,7 @@ func (c *Client) doRequest(req *http.Request, authToken *string) ([]byte, error)
 	}
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
