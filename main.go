937fc2a56b7fe96aefc82e29e38edd6e0d569228
Rodman <rodman@stuhlmuller.net>
2024-04-14 22:01:12 -0700

    tutorial - 22:58

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 		// TODO: Update this string with the published name of your provider.
 		// Also update the tfplugindocs generate command to either remove the
 		// -provider-name flag or set its value to the updated provider name.
-		Address: "hashicorp.local/hashicorp/casaos", // "registry.terraform.io/hashicorp/casaos",
+		Address: "hashicorp.local/rstuhlmuller/casaos", // "registry.terraform.io/hashicorp/casaos",
 		Debug:   debug,
 	}
 
